f19cd1b48d7347581d6f747d6f973faa48a94814
saviola24 <saviolafenitr@gmail.com>
2026-01-16 05:34:02 +0300

    creation files

---
+++ src/main/resources/new_shema.sql
@@ -0,0 +1,3 @@
+CREATE TYPE dish_type AS ENUM ('STARTER', 'MAIN', 'DESSERT');
+CREATE TYPE unit_type AS ENUM ('PCS', 'KG', 'L');
+CREATE TYPE ingredient_category AS ENUM ('VEGETABLE', 'MEAT', 'FRUIT', 'DAIRY', 'OTHER');
