668e777d90a8837696af9968c3b41ac4221bd49a
saviola24 <saviolafenitr@gmail.com>
2026-01-16 05:48:56 +0300

    insertion in DishIngredient

--- src/main/java/HEI/DishIngredient.java
+++ src/main/java/HEI/DishIngredient.java
@@ -1,5 +1,59 @@
 package HEI;
 
 public class DishIngredient {
-    
+    private int id;
+    private int idDish;
+    private int idIngredient;
+    private double quantityRequired;
+    private String unit;
+
+    public DishIngredient(int id, int idDish, int idIngredient, double quantityRequired, String unit) {
+        this.id = id;
+        this.idDish = idDish;
+        this.idIngredient = idIngredient;
+        this.quantityRequired = quantityRequired;
+        this.unit = unit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdDish() {
+        return idDish;
+    }
+
+    public void setIdDish(int idDish) {
+        this.idDish = idDish;
+    }
+
+    public int getIdIngredient() {
+        return idIngredient;
+    }
+
+    public void setIdIngredient(int idIngredient) {
+        this.idIngredient = idIngredient;
+    }
+
+    public double getQuantityRequired() {
+        return quantityRequired;
+    }
+
+    public void setQuantityRequired(double quantityRequired) {
+        this.quantityRequired = quantityRequired;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
 }
+
